7ce579dc65c6b7957a00c219a4f5864c76ddd048
pdc9922 <88790060+pdc9922@users.noreply.github.com>
2023-10-29 10:39:56 -0500

    Update jpds.js

--- jpds.js
+++ jpds.js
@@ -7,22 +7,25 @@ TG群：https://t.me/peckios
 脚本作者：鹏
 使用声明：脚本仅供参考，请勿转载与售卖！
 
-**************************************/
+**************************************
 
 [rewrite_local]
-^https?:\/\/api\.vesal\.cn\ $ url script-response-body https://raw.githubusercontent.com/pdc9922/QuantumultX/main/jpds.js
+^https?:\/\/api\.vesal\.cn $ url script-response-body https://raw.githubusercontent.com/pdc9922/QuantumultX/main/jpds.js
 
 **************************************
 
 [mitm]
 hostname = api.vesal.cn
 
-*************************************
+*************************************/
 
+var body = $response.body;
+var objc = JSON.parse(body);
 
-var body = $response.body
-    .replace(/\"isCharge\":\"\w+\"/g, "\"isCharge\":\"N"")
-    .replace(/\"ischarge\":\"\w+\"/g, "\"ischarge\":\"n\"");
-$done({
-    body
-});
+objc.data = {
+    "isCharge":"N",
+    "ischarge":"n"
+};
+
+body = JSON.stringify(objc);
+$done({ body });
