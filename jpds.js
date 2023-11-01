662df2f11c70599f5487cc2451dba22426159567
pdc9922 <88790060+pdc9922@users.noreply.github.com>
2023-11-01 10:34:11 -0500

    Update jpds.js

--- jpds.js
+++ jpds.js
@@ -10,7 +10,7 @@ TG群：https://t.me/peckios
 **************************************
 
 [rewrite_local]
-^https?:\/\/api.vesal.cn\/vesal-jiepao-prod\/ultimate\/app\/personalCenter\/getActiveComboList url script-response-body https://raw.githubusercontent.com/pdc9922/QuantumultX/main/jpds.js
+^https?:\/\/api.vesal.cn\/(ultimate|personalCenter)$ url script-response-body https://raw.githubusercontent.com/pdc9922/QuantumultX/main/jpds.js
 
 [mitm]
 hostname = api.vesal.cn
@@ -44,5 +44,5 @@ var objc = JSON.parse($response.body);
     "endTime": "2999-12-12",
     "comboCode": "ULTIMATE_ANATOMY_SVIP"
   }]
-};
+}
 $done({body : JSON.stringify(objc)});
