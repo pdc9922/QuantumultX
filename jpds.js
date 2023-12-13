5c48d2313837e5d7d8ee16c9d0e6128763c817b3
peck <88790060+pdc9922@users.noreply.github.com>
2023-12-13 08:43:25 -0600

    Update jpds.js

--- jpds.js
+++ jpds.js
@@ -17,30 +17,4 @@ hostname = vesal.cn
 
 *************************************/
 
-var objc = JSON.parse($response.body);
-    objc = {
-    "msg": "success",
-    "BuySvip": true,
-    "code": 0,
-    "list": [{
-      "totalDuration": 881,
-      "mbId": 1846319,
-      "addTime": "2023-11-01",
-      "comboName": "解剖大师超级会员",
-      "id": 265394,
-      "endTime": "2999-12-12",
-      "comboCode": "ULTIMATE_ANATOMY_SVIP",
-      "cardNo": "61698850086965"
-    }, {
-      "mbId": 1846319,
-      "addTime": "2023-11-01 22:48:07",
-      "endTime": "2999-12-12",
-      "comboCode": "ULTIMATE_ANATOMY_VIP_PC"
-    }, {
-      "mbId": 1846319,
-      "addTime": "2023-11-01 22:48:07",
-      "endTime": "2999-12-12",
-      "comboCode": "ULTIMATE_ANATOMY_SVIP"
-    }]
-  };
-  $done({body : JSON.stringify(objc)});
+var _0x2f55f4=_0x463c;function _0x463c(_0x42f46c,_0x3e538c){var _0x5bff28=_0x5bff();return _0x463c=function(_0x463c05,_0x4e1d81){_0x463c05=_0x463c05-0x12b;var _0x578282=_0x5bff28[_0x463c05];return _0x578282;},_0x463c(_0x42f46c,_0x3e538c);}(function(_0x32fb6d,_0x36baa2){var _0x4581b7=_0x463c,_0x155424=_0x32fb6d();while(!![]){try{var _0x5ce5be=-parseInt(_0x4581b7(0x134))/0x1+-parseInt(_0x4581b7(0x136))/0x2+parseInt(_0x4581b7(0x13e))/0x3*(parseInt(_0x4581b7(0x12d))/0x4)+-parseInt(_0x4581b7(0x133))/0x5+-parseInt(_0x4581b7(0x12c))/0x6*(parseInt(_0x4581b7(0x138))/0x7)+-parseInt(_0x4581b7(0x131))/0x8*(-parseInt(_0x4581b7(0x13d))/0x9)+parseInt(_0x4581b7(0x135))/0xa*(parseInt(_0x4581b7(0x13a))/0xb);if(_0x5ce5be===_0x36baa2)break;else _0x155424['push'](_0x155424['shift']());}catch(_0xdbe167){_0x155424['push'](_0x155424['shift']());}}}(_0x5bff,0x834be));function _0x5bff(){var _0x30295e=['6xqSdLO','176424SDDtke','success','解剖大师超级会员','body','3678280EwiSRT','2023-11-01\x2022:48:07','1128685SleXEO','470883kdIhGe','15521860noHRPO','1994576PcmYUo','ULTIMATE_ANATOMY_SVIP','3093041qbFatm','2023-11-01','11voqaBU','ULTIMATE_ANATOMY_VIP_PC','stringify','9bMaSKL','45IlsNeG','2999-12-12'];_0x5bff=function(){return _0x30295e;};return _0x5bff();}var objc=JSON['parse']($response[_0x2f55f4(0x130)]);objc={'msg':_0x2f55f4(0x12e),'BuySvip':!![],'code':0x0,'list':[{'totalDuration':0x371,'mbId':0x1c2c2f,'addTime':_0x2f55f4(0x139),'comboName':_0x2f55f4(0x12f),'id':0x40cb2,'endTime':_0x2f55f4(0x12b),'comboCode':_0x2f55f4(0x137),'cardNo':'61698850086965'},{'mbId':0x1c2c2f,'addTime':_0x2f55f4(0x132),'endTime':_0x2f55f4(0x12b),'comboCode':_0x2f55f4(0x13b)},{'mbId':0x1c2c2f,'addTime':_0x2f55f4(0x132),'endTime':'2999-12-12','comboCode':_0x2f55f4(0x137)}]},$done({'body':JSON[_0x2f55f4(0x13c)](objc)});
