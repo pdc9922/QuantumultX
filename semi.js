1caeeadf9043beea66b07d7b88d90c7bee802533
peck <88790060+pdc9922@users.noreply.github.com>
2024-05-02 11:03:24 +0800

    Update semi.js

--- semi.js
+++ semi.js
@@ -2,14 +2,15 @@
 色迷聚合
 
 [rewrite_local]
-http:\/\/api.sgapi004.xyz\/api\/applogin url script-response-body https://raw.githubusercontent.com/pdc9922/QuantumultX/main/semi.js
+http:\/\/api\.sgapi004\.xyz\/api\/applogin url script-response-body https://raw.githubusercontent.com/pdc9922/QuantumultX/main/semi.js
 
 [mitm] 
 hostname = api.sgapi004.xyz
 ********************/
 
-
-let obj = JSON.parse($response.body);
+let body = $response.body;
+let obj = JSON.parse(body);
+let url = $request.url;
 
 if ($request.url === "https://api.sgapi004.xyz/api/applogin") {
     obj.data.deleted_at = "2999-12-12 21:27:01";//
