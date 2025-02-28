cc9b3c3232576a71f36d1e6280bd8da6c7566852
peck <88790060+pdc9922@users.noreply.github.com>
2025-03-01 00:43:15 +0800

    btzs
    test

---
+++ btzs.js
@@ -0,0 +1,22 @@
+/******************************
+
+脚本名称：宝塔助手hook test
+脚本功能：本脚本仅供内部测试
+脚本作者：by鹏
+更新时间：2025-03-01
+
+*******************************/
+[rewrite_local]
+^https?:\/\/zsbt\.apisrv\.cn url script-response-body https://raw.githubusercontent.com/btzs.js
+
+[mitm]
+hostname = zsbt.apisrv.cn
+
+
+
+$notify("鹏哥提醒您脚本已注入成功!");
+var body = $response.body
+    .replace(/false/g, "true");
+$done({
+    body
+});
