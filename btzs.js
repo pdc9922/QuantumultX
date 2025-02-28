2e3cf7237591b46f69fd6b0261e761902825a01a
peck <88790060+pdc9922@users.noreply.github.com>
2025-03-01 00:45:30 +0800

    Update btzs.js

--- btzs.js
+++ btzs.js
@@ -5,13 +5,13 @@
 脚本作者：by鹏
 更新时间：2025-03-01
 
-*******************************/
+*******************************
 [rewrite_local]
 ^https?:\/\/zsbt\.apisrv\.cn url script-response-body https://raw.githubusercontent.com/pdc9922/QuantumultX/refs/heads/main/btzs.js
 
 [mitm]
 hostname = zsbt.apisrv.cn
-
+*******************************/
 
 
 $notify("鹏哥提醒您脚本已注入成功!");
